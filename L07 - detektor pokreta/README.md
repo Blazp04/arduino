4e1198ba77ad3778c572cbc81e5764b08ca05a17
Blaž Perić <blaz.peric4@gmail.com>
2023-01-18 16:23:23 +0100

    L07 - detektor pokreta

---
+++ L07 - detektor pokreta/README.md
@@ -0,0 +1,1 @@
+https://wokwi.com/projects/354205272718159873
---
+++ L07 - detektor pokreta/sketch.ino
@@ -0,0 +1,32 @@
+int senzorPokrata = 8; // definira da je na pin broj 8 povezan senzor pokreta
+int relayPin = 7;
+
+// stvaramo novu varijablu tipa podataka "bool"
+// u koju ćemo kasnije spremiti podatke o detekciji pokreta
+bool pokretDetektiran;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(senzorPokrata, INPUT);
+  pinMode(relayPin, INPUT);
+}
+
+void loop() {
+  // Sada provjeravamo kakvo je stanje senzora.
+  // Kada je pokret detektiran, stanje će biti HIGH
+  // u tome slučaju želimo našoj varijabli dati vrijednost "true"
+  // u koloko senzor nije aktiviran stanje će biti "false"
+  pokretDetektiran = digitalRead(senzorPokrata);
+
+  if (pokretDetektiran == true)
+  {
+    Serial.println("pokret detektiran");
+    digitalWrite(relayPin, HIGH);
+  }
+  else
+  {
+    Serial.println("Senzor deaktiviran");
+    digitalWrite(relayPin, LOW);
+
+  }
+}
